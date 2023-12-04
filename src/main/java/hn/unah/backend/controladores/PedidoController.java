5b21252b705f3f349cb9d809ee0d0d2952c6092a
Alejandro <arizaguirre19@gmail.com>
2023-12-03 20:01:50 -0600

    Endpoint para crear pedido

--- src/main/java/hn/unah/backend/controladores/PedidoController.java
+++ src/main/java/hn/unah/backend/controladores/PedidoController.java
@@ -2,11 +2,13 @@ package hn.unah.backend.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import hn.unah.backend.servicios.impl.PedidoServiceImpl;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import hn.unah.backend.modelos.pedidos;
 
 @RestController
@@ -15,6 +17,11 @@ public class PedidoController {
     
     @Autowired
     private PedidoServiceImpl pedidoServiceImpl;
+
+    @PostMapping("/crearPedido")
+    private pedidos crearPedidos(@RequestBody pedidos nvoPedidos){
+        return this.pedidoServiceImpl.crearPedidos(nvoPedidos);
+    }
 
     @GetMapping("/obtener/todos")
     public List<pedidos> obtenerTodos(){
